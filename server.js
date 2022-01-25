7bdb6b0504d66ec71e5d8dfd4b25045f36688763
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-01-25 16:00:31 -0600

    change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/homeworksubmissions", {
+mongoose.connect("mongodb://localhost/bigbudget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
